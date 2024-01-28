7e1ab3e33b9fb3339b9eea006f6552fbe3cd8ad8
pabletefatimarocio <pabletefatima9@gmail.com>
2024-01-28 20:59:42 -0300

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let initialY;
 const dibujar = (cursorX, cursorY) => {
     context.beginPath();
     context.moveTo(initialX, initialY);
-    context.lineWith = 20;
+    context.lineWidth = 15;
     context.strokeStyle = "#000";
     context.lineCap = "round";
     context.lineJoin = "round";
@@ -24,8 +24,20 @@ const dibujar = (cursorX, cursorY) => {
 const mouseDown = (evt) => {
     initialX = evt.offsetX;
     initialY = evt.offsetY;
-
     dibujar(initialX, initialY);
+    mainCanvas.addEventListener("mousemove", mouseMoving);
+};
+
+
+
+const mouseMoving = (evt) => {
+    dibujar(evt.offsetX, evt.offsetY);
+};
+
+
+const mouseUp = () => {
+    mainCanvas.removeEventListener("mousemove", mouseMoving);
 };
 
 mainCanvas.addEventListener("mousedown", mouseDown);
+mainCanvas.addEventListener("mouseup", mouseUp);
