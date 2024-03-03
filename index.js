b89b5a67e207b6b0ae0ea47c0a5b32935c955437
pabletefatimarocio <pabletefatima9@gmail.com>
2024-03-03 16:53:48 -0300

    input

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const mainCanvas = document.getElementById("main-canvas");
 const context = mainCanvas.getContext("2d");
-
+const  brushSizeInput = document.getElementById("brush-size");
 
 let initialX;
 let initialY;
+let brushSize = parseInt(brushSizeInput.value);
+
+
 
 const dibujar = (cursorX, cursorY) => {
     context.beginPath();
     context.moveTo(initialX, initialY);
-    context.lineWidth = 15;
+    context.lineWidth = brushSize;
     context.strokeStyle = "#000";
     context.lineCap = "round";
     context.lineJoin = "round";
@@ -19,7 +22,9 @@ const dibujar = (cursorX, cursorY) => {
     initialY = cursorY;
 };
 
-
+const cambiarTamañoPincel = () => {
+    brushSize = parseInt(brushSizeInput.value);
+};
 
 const mouseDown = (evt) => {
     initialX = evt.offsetX;
@@ -39,5 +44,6 @@ const mouseUp = () => {
     mainCanvas.removeEventListener("mousemove", mouseMoving);
 };
 
+brushSizeInput.addEventListener("input", cambiarTamañoPincel);
 mainCanvas.addEventListener("mousedown", mouseDown);
 mainCanvas.addEventListener("mouseup", mouseUp);
